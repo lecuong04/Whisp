cbe3b812d2868fde9809140db5fdb56729ce4fa1
Le Ngoc Cuong <lengoccuong02042004@gmail.com>
2025-05-01 12:47:13 +0700

    .

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     ndkVersion = "28.0.13004108"
 
     compileOptions {
-		isCoreLibraryDesugaringEnabled = true
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -60,7 +60,7 @@ android {
 dependencies {
     implementation("androidx.multidex:multidex:2.0.1")
     implementation("androidx.appcompat:appcompat:1.3.0")
-	coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
