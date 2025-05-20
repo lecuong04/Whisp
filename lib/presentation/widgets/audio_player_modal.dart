e1641062725ed3ce79dd5c2cc66ae4b5e809e6de
Le Ngoc Cuong <lengoccuong02042004@gmail.com>
2025-05-20 23:45:15 +0700

    .

--- lib/presentation/widgets/audio_player_modal.dart
+++ lib/presentation/widgets/audio_player_modal.dart
@@ -69,13 +69,6 @@ class _AudioPlayerModalState extends State<AudioPlayerModal> {
     super.dispose();
   }
 
-  static String formatDuration(Duration duration) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
-    final minutes = twoDigits(duration.inMinutes.remainder(60));
-    final seconds = twoDigits(duration.inSeconds.remainder(60));
-    return '$minutes:$seconds';
-  }
-
   @override
   Widget build(BuildContext context) {
     const iconSize = 32.0;
--- lib/utils/helpers.dart
+++ lib/utils/helpers.dart
@@ -66,3 +66,10 @@ String getFileNameFromSupabaseStorage(String url) {
         .replaceFirstMapped(RegExp('^\\d+_', unicode: true), (match) => ''),
   );
 }
+
+String formatDuration(Duration duration) {
+  String twoDigits(int n) => n.toString().padLeft(2, '0');
+  final minutes = twoDigits(duration.inMinutes.remainder(60));
+  final seconds = twoDigits(duration.inSeconds.remainder(60));
+  return '$minutes:$seconds';
+}
